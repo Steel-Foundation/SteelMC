461ee84a81a2cea3bdb4afaafc5066a1c9f9b850
JunkyDeveloper <junkydeveloper@gmail.com>
2026-02-05 01:32:25 +0100

    cargo fmt

--- steel-core/src/behavior/blocks/mod.rs
+++ steel-core/src/behavior/blocks/mod.rs
@@ -3,11 +3,11 @@
 //! The actual behavior registration is auto-generated from classes.json.
 //! See `src/generated/behaviors.rs` for the generated registration code.
 
+mod barrel_block;
+mod candle_block;
 mod chain_block;
 mod copper_bars_block;
 mod copper_chain_block;
-mod barrel_block;
-mod candle_block;
 mod crafting_table_block;
 mod crop_block;
 mod end_portal_frame_block;
@@ -16,15 +16,15 @@ mod fence_block;
 mod iron_bars_block;
 mod redstone_torch_block;
 mod rotated_pillar_block;
-mod wall_block;
 mod sign_block;
 mod torch_block;
+mod wall_block;
 
+pub use barrel_block::BarrelBlock;
+pub use candle_block::CandleBlock;
 pub use chain_block::ChainBlock;
 pub use copper_bars_block::WeatheringCopperBarsBlock;
 pub use copper_chain_block::WeatheringCopperChainBlock;
-pub use barrel_block::BarrelBlock;
-pub use candle_block::CandleBlock;
 pub use crafting_table_block::CraftingTableBlock;
 pub use crop_block::CropBlock;
 pub use end_portal_frame_block::EndPortalFrameBlock;
@@ -33,8 +33,8 @@ pub use fence_block::FenceBlock;
 pub use iron_bars_block::IronBarsBlock;
 pub use redstone_torch_block::{RedstoneTorchBlock, RedstoneWallTorchBlock};
 pub use rotated_pillar_block::RotatedPillarBlock;
-pub use wall_block::WallBlock;
 pub use sign_block::{
     CeilingHangingSignBlock, StandingSignBlock, WallHangingSignBlock, WallSignBlock,
 };
 pub use torch_block::{TorchBlock, WallTorchBlock};
+pub use wall_block::WallBlock;
