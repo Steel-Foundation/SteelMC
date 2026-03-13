f2a5d715d8707a092490f07d106e66214b7c1097
CuzImClicks <bruno778.whiteelfie@gmail.com>
2026-03-13 20:45:00 +0100

    nether sprouts

--- steel-core/build/blocks.rs
+++ steel-core/build/blocks.rs
@@ -92,6 +92,7 @@ pub fn build(blocks: &[BlockClass]) -> String {
     let mut fence_blocks = Vec::new();
     let mut flower_blocks = Vec::new();
     let mut liquid_blocks = Vec::new();
+    let mut nether_sprouts_blocks = Vec::new();
     let mut nether_wart_blocks = Vec::new();
     let mut redstone_torch_blocks = Vec::new();
     let mut redstone_wall_torch_blocks = Vec::new();
@@ -163,6 +164,7 @@ pub fn build(blocks: &[BlockClass]) -> String {
                     to_const_ident(block.fluid.as_ref().expect("LiquidBlock must have a fluid"));
                 liquid_blocks.push((const_ident, fluid_ident));
             }
+            "NetherSproutsBlock" => nether_sprouts_blocks.push(const_ident),
             "NetherWartBlock" => nether_wart_blocks.push(const_ident),
             "RedstoneTorchBlock" => redstone_torch_blocks.push(const_ident),
             "RedstoneWallTorchBlock" => redstone_wall_torch_blocks.push(const_ident),
@@ -205,6 +207,7 @@ pub fn build(blocks: &[BlockClass]) -> String {
     let farmland_type = Ident::new("FarmlandBlock", Span::call_site());
     let fence_type = Ident::new("FenceBlock", Span::call_site());
     let flower_type = Ident::new("FlowerBlock", Span::call_site());
+    let nether_sprouts_type = Ident::new("NetherSproutsBlock", Span::call_site());
     let nether_wart_type = Ident::new("NetherWartBlock", Span::call_site());
     let pillar_type = Ident::new("RotatedPillarBlock", Span::call_site());
     let redstone_torch_type = Ident::new("RedstoneTorchBlock", Span::call_site());
@@ -272,6 +275,8 @@ pub fn build(blocks: &[BlockClass]) -> String {
         });
         quote! { #(#registrations)* }
     };
+    let nether_sprouts_registrations =
+        generate_registrations(nether_sprouts_blocks.iter(), &nether_sprouts_type);
     let nether_wart_registrations =
         generate_registrations(nether_wart_blocks.iter(), &nether_wart_type);
     let pillar_registrations = generate_registrations(rotated_pillar_blocks.iter(), &pillar_type);
@@ -316,7 +321,7 @@ pub fn build(blocks: &[BlockClass]) -> String {
         use steel_registry::{sound_events, vanilla_blocks, vanilla_fluids};
 
         use crate::behavior::blocks::{
-            crops::{ AzaleaBlock, BambooSaplingBlock, BambooStalkBlock, BeetrootBlock, CactusBlock, CactusFlowerBlock, CropBlock, FlowerBlock, NetherWartBlock, SeagrassBlock, TallSeagrassBlock, TorchflowerBlock},
+            crops::{ AzaleaBlock, BambooSaplingBlock, BambooStalkBlock, BeetrootBlock, CactusBlock, CactusFlowerBlock, CropBlock, FlowerBlock, NetherSproutsBlock, NetherWartBlock, SeagrassBlock, TallSeagrassBlock, TorchflowerBlock},
             BarrelBlock,
             ButtonBlock,
             CandleBlock,
@@ -357,6 +362,7 @@ pub fn build(blocks: &[BlockClass]) -> String {
             #fence_registrations
             #flower_registrations
             #liquid_registrations
+            #nether_sprouts_registrations
             #nether_wart_registrations
             #pillar_registrations
             #redstone_torch_registrations
---
+++ steel-core/src/behavior/blocks/crops/nether_sprouts.rs
@@ -0,0 +1,76 @@
+use steel_registry::{
+    REGISTRY,
+    blocks::{BlockRef, block_state_ext::BlockStateExt},
+    vanilla_block_tags::{self},
+    vanilla_blocks,
+};
+use steel_utils::{BlockPos, BlockStateId};
+
+use crate::{
+    behavior::{
+        BlockBehaviour, BlockPlaceContext,
+        blocks::crops::{
+            Vegetation,
+            vegetation_block::{vegetation_can_survive, vegetation_update_shape},
+        },
+    },
+    world::World,
+};
+
+/// Behavior for Nether Sprouts
+pub struct NetherSproutsBlock {
+    block: BlockRef,
+}
+
+impl NetherSproutsBlock {
+    /// Creates a new Nether Sprout Block Behavior
+    #[must_use]
+    pub const fn new(block: BlockRef) -> Self {
+        Self { block }
+    }
+}
+
+impl BlockBehaviour for NetherSproutsBlock {
+    fn get_state_for_placement(&self, context: &BlockPlaceContext<'_>) -> Option<BlockStateId> {
+        if self.may_place_on(
+            context.world.get_block_state(&context.relative_pos.below()),
+            context.world,
+            context.relative_pos.below(),
+        ) {
+            Some(self.block.default_state())
+        } else {
+            None
+        }
+    }
+
+    fn update_shape(
+        &self,
+        state: BlockStateId,
+        world: &World,
+        pos: BlockPos,
+        _direction: steel_utils::Direction,
+        _neighbor_pos: BlockPos,
+        _neighbor_state: BlockStateId,
+    ) -> BlockStateId {
+        vegetation_update_shape(self, state, world, pos)
+    }
+
+    fn can_survive(&self, state: BlockStateId, world: &World, pos: BlockPos) -> bool {
+        vegetation_can_survive(self, state, world, pos)
+    }
+}
+
+impl Vegetation for NetherSproutsBlock {
+    fn may_place_on(&self, state: BlockStateId, _world: &World, _pos: BlockPos) -> bool {
+        state.get_block() == vanilla_blocks::SOUL_SOIL
+            || REGISTRY
+                .blocks
+                .is_in_tag(state.get_block(), &vanilla_block_tags::NYLIUM_TAG)
+            || {
+                steel_registry::REGISTRY
+                    .blocks
+                    .is_in_tag(state.get_block(), &vanilla_block_tags::DIRT_TAG)
+                    || state.get_block() == vanilla_blocks::FARMLAND
+            }
+    }
+}
