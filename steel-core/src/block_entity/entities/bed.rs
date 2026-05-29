4d1ab02b036a192d169fa120cc81205450a31e43
Joniii11 <88550325+Joniii11@users.noreply.github.com>
2026-05-29 13:02:29 +0200

    Resolve bed colors from block refs

--- steel-core/src/block_entity/entities/bed.rs
+++ steel-core/src/block_entity/entities/bed.rs
@@ -8,7 +8,7 @@ use simdnbt::owned::NbtCompound;
 use steel_registry::block_entity_type::BlockEntityTypeRef;
 use steel_registry::blocks::block_state_ext::BlockStateExt;
 use steel_registry::loot_table::DyeColor;
-use steel_registry::vanilla_block_entity_types;
+use steel_registry::{vanilla_block_entity_types, vanilla_blocks};
 use steel_utils::{BlockPos, BlockStateId};
 
 use crate::block_entity::BlockEntity;
@@ -54,30 +54,45 @@ impl BedBlockEntity {
     }
 
     fn color_from_state(state: BlockStateId) -> DyeColor {
-        match state.get_block().key.path.as_ref() {
-            "white_bed" => DyeColor::White,
-            "orange_bed" => DyeColor::Orange,
-            "magenta_bed" => DyeColor::Magenta,
-            "light_blue_bed" => DyeColor::LightBlue,
-            "yellow_bed" => DyeColor::Yellow,
-            "lime_bed" => DyeColor::Lime,
-            "pink_bed" => DyeColor::Pink,
-            "gray_bed" => DyeColor::Gray,
-            "light_gray_bed" => DyeColor::LightGray,
-            "cyan_bed" => DyeColor::Cyan,
-            "purple_bed" => DyeColor::Purple,
-            "blue_bed" => DyeColor::Blue,
-            "brown_bed" => DyeColor::Brown,
-            "green_bed" => DyeColor::Green,
-            "red_bed" => DyeColor::Red,
-            "black_bed" => DyeColor::Black,
-            other => {
-                log::warn!(
-                    "unknown bed block '{}' for BedBlockEntity color; defaulting to white",
-                    other
-                );
-                DyeColor::White
-            }
+        let block = state.get_block();
+        if block == &vanilla_blocks::WHITE_BED {
+            DyeColor::White
+        } else if block == &vanilla_blocks::ORANGE_BED {
+            DyeColor::Orange
+        } else if block == &vanilla_blocks::MAGENTA_BED {
+            DyeColor::Magenta
+        } else if block == &vanilla_blocks::LIGHT_BLUE_BED {
+            DyeColor::LightBlue
+        } else if block == &vanilla_blocks::YELLOW_BED {
+            DyeColor::Yellow
+        } else if block == &vanilla_blocks::LIME_BED {
+            DyeColor::Lime
+        } else if block == &vanilla_blocks::PINK_BED {
+            DyeColor::Pink
+        } else if block == &vanilla_blocks::GRAY_BED {
+            DyeColor::Gray
+        } else if block == &vanilla_blocks::LIGHT_GRAY_BED {
+            DyeColor::LightGray
+        } else if block == &vanilla_blocks::CYAN_BED {
+            DyeColor::Cyan
+        } else if block == &vanilla_blocks::PURPLE_BED {
+            DyeColor::Purple
+        } else if block == &vanilla_blocks::BLUE_BED {
+            DyeColor::Blue
+        } else if block == &vanilla_blocks::BROWN_BED {
+            DyeColor::Brown
+        } else if block == &vanilla_blocks::GREEN_BED {
+            DyeColor::Green
+        } else if block == &vanilla_blocks::RED_BED {
+            DyeColor::Red
+        } else if block == &vanilla_blocks::BLACK_BED {
+            DyeColor::Black
+        } else {
+            log::warn!(
+                "unknown bed block '{}' for BedBlockEntity color; defaulting to white",
+                block.key
+            );
+            DyeColor::White
         }
     }
 }
