39e17c8bb3cdd3e2fd62a39376ed635bca4d99ab
Joniii11 <88550325+Joniii11@users.noreply.github.com>
2026-06-20 21:49:48 +0200

    feat: finish carpet

--- steel-core/src/behavior/blocks/vegetation/carpet_block.rs
+++ steel-core/src/behavior/blocks/vegetation/carpet_block.rs
@@ -1,15 +1,16 @@
 use steel_macros::block_behavior;
 use steel_registry::blocks::block_state_ext::BlockStateExt;
-use steel_utils::{BlockPos, BlockStateId};
+use steel_registry::vanilla_blocks;
+use steel_utils::{BlockPos, BlockStateId, Direction};
 
 use crate::behavior::block::BlockBehavior;
+use crate::behavior::blocks::vegetation::vegetation_block::vegetation_update_shape;
 use crate::behavior::context::BlockPlaceContext;
-use crate::world::LevelReader;
+use crate::world::{LevelReader, ScheduledTickAccess};
 
 use super::{BlockRef, default_surviving_state};
 
-/// Vanilla `CarpetBlock` survival.
-// TODO: Implement full vanilla behavior beyond can_survive.
+/// Vanilla `CarpetBlock` survival and shape updates.
 #[block_behavior]
 pub struct CarpetBlock {
     block: BlockRef,
@@ -24,6 +25,19 @@ impl CarpetBlock {
 }
 
 impl BlockBehavior for CarpetBlock {
+    fn update_shape(
+        &self,
+        state: BlockStateId,
+        world: &dyn ScheduledTickAccess,
+        pos: BlockPos,
+        _direction: Direction,
+        _neighbor_pos: BlockPos,
+        _neighbor_state: BlockStateId,
+    ) -> BlockStateId {
+        // idk if this is good cause the name is vegetation_update_shape but carpet is not vegetation lol
+        vegetation_update_shape(self, state, world, pos)
+    }
+
     fn can_survive(&self, _state: BlockStateId, world: &dyn LevelReader, pos: BlockPos) -> bool {
         !world.get_block_state(pos.below()).is_air()
     }
