451441e1587098f9eacad1cac99a20ad3353c25a
coco875 <59367621+coco875@users.noreply.github.com>
2026-07-06 00:20:13 +0200

    Update biomes.rs

--- steel-registry/build/biomes.rs
+++ steel-registry/build/biomes.rs
@@ -12,7 +12,8 @@ use crate::generator_functions::{
 use heck::ToShoutySnakeCase;
 use proc_macro2::{Ident, Span, TokenStream};
 use quote::quote;
-use serde::{Deserialize, Serialize};
+use serde::de::Error as DeError;
+use serde::{Deserialize, Deserializer, Serialize};
 use serde_json::Value;
 use steel_utils::Identifier;
 
@@ -671,7 +672,6 @@ pub(crate) fn build() -> TokenStream {
                 id: OnceLock::new(),
             });
         });
-        let biome_ident = Ident::new(&biome_name.to_shouty_snake_case(), Span::call_site());
         register_stream.extend(quote! {
             registry.register(&#biome_ident);
         });
