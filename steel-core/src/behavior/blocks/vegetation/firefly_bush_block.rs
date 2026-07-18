5e53738bf115ab663ef8df561539477af0663e9a
Greened <greeneddev@gmail.com>
2026-07-18 11:35:09 +0400

    feat: Finalize Firefly Bush Block implementation

--- steel-core/src/behavior/blocks/vegetation/firefly_bush_block.rs
+++ steel-core/src/behavior/blocks/vegetation/firefly_bush_block.rs
@@ -1,15 +1,20 @@
+use rand::Rng;
+use std::sync::Arc;
 use steel_macros::block_behavior;
 use steel_registry::vanilla_block_tags::BlockTag;
+use steel_utils::types::UpdateFlags;
 use steel_utils::{BlockPos, BlockStateId};
 
-use crate::behavior::block::BlockBehavior;
+use crate::behavior::blocks::vegetation::bonemealable::{
+    BonemealAction, find_spreadable_neighbor_pos, has_spreadable_neighbor_pos,
+};
 use crate::behavior::context::BlockPlaceContext;
-use crate::world::LevelReader;
+use crate::behavior::{block::BlockBehavior, blocks::vegetation::bonemealable::Bonemealable};
+use crate::world::{LevelReader, World};
 
 use super::{BlockRef, default_surviving_state, survives_on_tag};
 
 /// Vanilla `FireflyBushBlock` survival.
-// TODO: Implement particles, sounds, bonemeal, and the rest of vanilla behavior.
 #[block_behavior]
 pub struct FireflyBushBlock {
     block: BlockRef,
@@ -30,5 +35,49 @@ impl BlockBehavior for FireflyBushBlock {
 
     fn get_state_for_placement(&self, context: &BlockPlaceContext<'_>) -> Option<BlockStateId> {
         default_surviving_state(self.block, self, context)
+    }
+    fn as_bonemealable(&self) -> Option<&dyn super::bonemealable::Bonemealable> {
+        Some(self)
+    }
+}
+impl Bonemealable for FireflyBushBlock {
+    fn is_valid_bonemeal_target(
+        &self,
+        state: BlockStateId,
+        world: &dyn LevelReader,
+        pos: BlockPos,
+    ) -> bool {
+        has_spreadable_neighbor_pos(world, pos, state)
+    }
+
+    fn is_bonemeal_success(
+        &self,
+        _state: BlockStateId,
+        _world: &Arc<World>,
+        _rng: &mut dyn Rng,
+        _pos: BlockPos,
+    ) -> bool {
+        true
+    }
+
+    fn perform_bonemeal(
+        &self,
+        state: BlockStateId,
+        world: &Arc<World>,
+        _rng: &mut dyn Rng,
+        pos: BlockPos,
+    ) {
+        let Some(block_pos) = find_spreadable_neighbor_pos(world, pos, state) else {
+            return;
+        };
+        world.set_block(
+            block_pos,
+            self.block.default_state(),
+            UpdateFlags::UPDATE_ALL,
+        );
+    }
+
+    fn bonemeal_action_type(&self) -> BonemealAction {
+        BonemealAction::NeighborSpreader
     }
 }
