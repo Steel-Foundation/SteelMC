7e760040384864a8fe9ed26148e96c802d4c6909
ChocoDev <camgiang1983@gmail.com>
2026-08-07 12:38:48 +0700

    formatiing

--- steel-core/src/behavior/blocks/decoration/end_rod_block.rs
+++ steel-core/src/behavior/blocks/decoration/end_rod_block.rs
@@ -1,9 +1,9 @@
 //! Vanilla `EndRodBlock` behavior.
 
 use steel_macros::block_behavior;
+use steel_registry::blocks::BlockRef;
 use steel_registry::blocks::block_state_ext::BlockStateExt;
 use steel_registry::blocks::properties::{BlockStateProperties, EnumProperty};
-use steel_registry::blocks::BlockRef;
 use steel_utils::{BlockStateId, Direction};
 
 use crate::behavior::{BlockBehavior, BlockPlaceContext};
@@ -27,17 +27,14 @@ impl EndRodBlock {
 impl BlockBehavior for EndRodBlock {
     fn get_state_for_placement(&self, context: &BlockPlaceContext<'_>) -> Option<BlockStateId> {
         let clicked_face = context.clicked_face();
-        let adjacent_pos = context
-            .place_pos()
-            .relative(clicked_face.opposite());
+        let adjacent_pos = context.place_pos().relative(clicked_face.opposite());
         let adjacent = context.world.get_block_state(adjacent_pos);
-        let facing = if adjacent.get_block() == self.block
-            && adjacent.get_value(&FACING) == clicked_face
-        {
-            clicked_face.opposite()
-        } else {
-            clicked_face
-        };
+        let facing =
+            if adjacent.get_block() == self.block && adjacent.get_value(&FACING) == clicked_face {
+                clicked_face.opposite()
+            } else {
+                clicked_face
+            };
 
         Some(self.block.default_state().set_value(&FACING, facing))
     }
