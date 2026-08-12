0b127bd5c41f978af83d715f4bc3c21376b393dd
starwarslevi <309551843+starwarslevi@users.noreply.github.com>
2026-08-12 16:33:55 +1000

    update sheep entity for mob structure reorganization

--- steel-core/src/entity/entities/mod.rs
+++ steel-core/src/entity/entities/mod.rs
@@ -3,13 +3,11 @@
 pub mod mobs;
 pub mod objects;
 mod raw;
-mod sheep;
 
-pub use mobs::passive::{CowEntity, PigEntity};
+pub use mobs::passive::{CowEntity, PigEntity, SheepEntity};
 pub use objects::display_ui::{BlockDisplayEntity, ItemFrameEntity, LeashFenceKnotEntity};
 pub use objects::explosives::EndCrystalEntity;
 pub use objects::items::{ExperienceOrbEntity, ItemEntity};
 pub use objects::projectiles::{EnderPearlEntity, FireworkRocketEntity};
 pub use objects::vehicles::ChestMinecartEntity;
 pub use raw::RawEntity;
-pub use sheep::SheepEntity;
