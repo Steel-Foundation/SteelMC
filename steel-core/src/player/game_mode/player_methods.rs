809694ada46dbc3a1accb8ea92179a9223e4cbbb
Greened <greeneddev@gmail.com>
2026-08-19 00:00:34 +0400

    fix: magic number

--- steel-core/src/player/game_mode/player_methods.rs
+++ steel-core/src/player/game_mode/player_methods.rs
@@ -8,6 +8,8 @@ use super::{
 };
 use crate::behavior::blocks::PowderSnowBlock;
 
+const SURVIVAL_DEFAULT_BLOCK_INTERACTION_RANGE: f64 = 4.5;
+
 impl Player {
     /// Sets the player's game mode and notifies the client.
     ///
@@ -215,13 +217,12 @@ impl Player {
         self.is_within_block_interaction_range_with_buffer(pos, 1.0)
     }
     /// Vanilla `player.blockInteractionRange()`
-    /// vanilla default is 4.5
     #[must_use]
     pub fn block_interaction_range(&self) -> f64 {
         self.attributes()
             .lock()
             .get_value(vanilla_attributes::BLOCK_INTERACTION_RANGE)
-            .unwrap_or(4.5)
+            .unwrap_or(SURVIVAL_DEFAULT_BLOCK_INTERACTION_RANGE)
     }
 
     /// Returns true if player is within block interaction range plus a vanilla buffer.
