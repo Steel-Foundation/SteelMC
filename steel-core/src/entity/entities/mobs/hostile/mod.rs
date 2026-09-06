9b9061c0f2b1abcaed8bd4909ed20a8e8a0fce70
levi <starwarslevi@proton.me>
2026-09-06 12:39:40 +1000

    Remove duplicate monster module declaration from hostile mod.rs

--- steel-core/src/entity/entities/mobs/hostile/mod.rs
+++ steel-core/src/entity/entities/mobs/hostile/mod.rs
@@ -2,8 +2,6 @@
 //!
 //! Those mobs are aggressive creatures that attack players on sight.
 
-/// Hostile monster entity module.
-pub mod monster;
 /// Hostile endermite entity module.
 pub mod endermite;
 pub use endermite::EndermiteEntity;
