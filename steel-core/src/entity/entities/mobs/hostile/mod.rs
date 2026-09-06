d8e3f86e1c13a791d309b28c603132e27ca24209
levi <starwarslevi@proton.me>
2026-09-06 12:34:34 +1000

    Add missing monster module declaration in hostile mod.rs

--- steel-core/src/entity/entities/mobs/hostile/mod.rs
+++ steel-core/src/entity/entities/mobs/hostile/mod.rs
@@ -2,6 +2,8 @@
 //!
 //! Those mobs are aggressive creatures that attack players on sight.
 
+/// Hostile monster entity module.
+pub mod monster;
 /// Hostile endermite entity module.
 pub mod endermite;
 pub use endermite::EndermiteEntity;
