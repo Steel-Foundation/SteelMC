5e5deec823d380c4dcc9a21acd392670d9b47c69
Ezra Clintoc <140189732+ezraclintoc@users.noreply.github.com>
2026-09-07 15:00:03 +0000

    fix: Move GrindstoneBlock import to be alphabetical

--- steel-core/src/behavior/blocks/container/mod.rs
+++ steel-core/src/behavior/blocks/container/mod.rs
@@ -13,5 +13,5 @@ pub use beehive_block::BeehiveBlock;
 pub use chiseled_bookshelf_block::ChiseledBookShelfBlock;
 pub use crafting_table_block::CraftingTableBlock;
 pub use ender_chest_block::EnderChestBlock;
-pub use grindstone_block::GrindstoneBlock;
 pub use furnace_block::{BlastFurnaceBlock, FurnaceBlock, SmokerBlock};
+pub use grindstone_block::GrindstoneBlock;
