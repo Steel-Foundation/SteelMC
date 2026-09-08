00a6812bffcf138856b3fe5a3a9567c39305a557
JunkyDeveloper <junkydeveloper@gmail.com>
2026-09-08 23:46:38 +0200

    fix only_op_can_set_nbt

--- steel-registry/build/entities.rs
+++ steel-registry/build/entities.rs
@@ -29,6 +29,7 @@ struct EntityTypeEntry {
     class_hierarchy: Vec<ClassHierarchyEntry>,
     #[serde(default = "default_can_serialize")]
     can_serialize: bool,
+    only_op_can_set_nbt: bool,
     #[serde(default)]
     flags: Option<FlagsEntry>,
     #[serde(default)]
@@ -180,7 +181,7 @@ pub(crate) fn build() -> TokenStream {
         let allowed_in_peaceful = entity_type.allowed_in_peaceful;
         let can_spawn_far = entity_type.can_spawn_far_from_player;
         let can_serialize = entity_type.can_serialize;
-        let only_op_can_set_nbt = vanilla_only_op_can_set_nbt(entity_type_key);
+        let only_op_can_set_nbt = entity_type.only_op_can_set_nbt;
         let is_abstract_boat = entity_type
             .class_hierarchy
             .iter()
