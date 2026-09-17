8423453775deb5849d710892c00d92fe6023f71a
darkstarworks <darkstarworks@gmail.com>
2026-09-18 04:03:28 +0700

    docs: point the turtle leash TODO at the hook's name

--- steel-core/src/entity/entities/mobs/passive/turtle/traits.rs
+++ steel-core/src/entity/entities/mobs/passive/turtle/traits.rs
@@ -267,7 +267,7 @@ impl Animal for TurtleEntity {
     }
 }
 
-// TODO(leash): turtles refuse a lead once Mob::can_be_leashed lands.
+// TODO(leash): turtles refuse a lead once Mob::mob_can_be_leashed lands.
 impl Mob for TurtleEntity {
     fn mob_base(&self) -> &MobBase {
         &self.mob_base
