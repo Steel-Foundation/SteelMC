06f4157ca2325d7cab7932d2e3acf96d5b1e2f36
Greened <greeneddev@gmail.com>
2026-09-21 00:28:51 +0400

    chore: fix formatting

--- steel-core/src/behavior/block/mod.rs
+++ steel-core/src/behavior/block/mod.rs
@@ -5,10 +5,10 @@ use std::sync::{Arc, Weak};
 use glam::DVec3;
 use rand::rngs::ThreadRng;
 use smallvec::SmallVec;
+use steel_registry::DyeColor;
 use steel_registry::block_entity_type::BlockEntityTypeRef;
 use steel_registry::blocks::BlockRef;
 use steel_registry::blocks::block_state_ext::BlockStateExt;
-use steel_registry::DyeColor;
 use steel_registry::blocks::properties::{BlockStateProperties, Direction};
 use steel_registry::blocks::shapes::{
     BooleanOp, ShapeChannel, SupportType, VoxelShape, is_block_local_face_sturdy,
